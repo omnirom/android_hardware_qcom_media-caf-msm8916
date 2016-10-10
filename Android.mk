affed374e340fe17dc463f5681ca6418853e5460
Max Weninger <max.weninger@gmail.com>
2016-10-10 03:24:16 +0000

    bring back TARGET_QCOM_MEDIA_VARIANT

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf-msm8916)
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
